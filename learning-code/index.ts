de215861bf358a33632083f31559ddbe9a7fe8d0
bernat alcudia <bernatalcudia@gmail.com>
2025-02-12 13:02:34 +0100

    deleted empty lines

--- learning-code/index.ts
+++ learning-code/index.ts
@@ -366,4 +366,3 @@ const cats: Record<CatName, CatInfo> = {
 type T0 = Exclude<"a" | "b" | "c", "a">;//b | c
 type T1 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>;//c
 type T2 = Exclude<string | number | (() => void), Function>; // string | number
-
