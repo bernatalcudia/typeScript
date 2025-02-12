3164a2f1ffb5b52415b2f6c5e3063f83c795ba8e
bernat alcudia <bernatalcudia@gmail.com>
2025-02-12 12:57:02 +0100

    added  Utility Types:Record

--- learning-code/index.ts
+++ learning-code/index.ts
@@ -349,3 +349,16 @@ const todo2: Readonly<Todo> = {
     description: "souls",
     completed: true,
 };
+//Record
+interface CatInfo {
+    age: number;
+    breed: string;
+}
+
+type CatName = "miffy" | "boris" | "mordred";
+
+const cats: Record<CatName, CatInfo> = {
+    miffy: { age: 10, breed: "Persian" },
+    boris: { age: 5, breed: "Maine Coon" },
+    mordred: { age: 16, breed: "British Short" }
+}
